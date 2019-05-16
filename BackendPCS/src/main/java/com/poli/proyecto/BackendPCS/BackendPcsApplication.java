52015b9e6d274b3b0dee26a559f1733d14f2073b
Miguel Campuzano <miguel.campuzano@accenture.com>
2019-05-15 21:14:11 -0500

    Primer commit

---
+++ BackendPCS/src/main/java/com/poli/proyecto/BackendPCS/BackendPcsApplication.java
@@ -0,0 +1,13 @@
+package com.poli.proyecto.BackendPCS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPcsApplication.class, args);
+	}
+
+}
